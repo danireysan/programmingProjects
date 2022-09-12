d814682fd324049670018bec9b4a10f4b1a78261
danireysan <danireysan@gmail.com>
2022-09-12 14:21:10 +0000

    javascript

---
+++ binary_search_algo/js_bin_search/iterative_method.js
@@ -0,0 +1,14 @@
+const start = () => {
+    console.log(" OWOOOOOOOOO")
+}
+
+// function main() {
+//     const array = [1,3,5,7,9,11,13]
+//     var target = 0
+//     console.log(" HELLLLLOOOOO")
+// }
+
+// function binarySearch(target, array) {
+//     high = array.lenght
+//     console.log(high)
+// }
